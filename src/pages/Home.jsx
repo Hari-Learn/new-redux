bd2d4d059afc39caae8b26c9f29f19ffb33e6379
Hari-repo <hariharanuk810@gmail.com>
2025-11-14 21:23:36 +0530

    Home service page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,12 @@ const Home = () => {
     return(
         <div>
             <h1>Home Page</h1>
+            <h2>React Lifecycle Methods - Class Components</h2>
+            <h6>In class components, React provides special methods called lifecycle methods that run at
+specific points in a component’s life:
+1. Mounting → When the component is first added to the DOM.
+2. Updating → When the component re-renders (because props or state changed).
+3. Unmounting → When the component is removed from the DOM.</h6>
         </div>
     )
 }
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Service = () => {
     return(
         <div>
-            <h3>Service page</h3>
+            <h1>Service page</h1>
         </div>
     )
 }
